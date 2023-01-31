cac2008528f2a449d6891ee5645ac9c3146d3409
rafiatha09 <92040855+rafiatha09@users.noreply.github.com>
2023-01-31 10:52:18 +0700

    Delete LocalD directory

--- LocalD/Next-Testing/tutorial-next/component/footer.js
+++
@@ -1,8 +0,0 @@
-import styles from '../styles/Footer.module.css'
-export default function Footer(){
-    return(
-        <div className={styles.container}>
-            <p className={styles.title}>made with passion @rafiatha11</p>
-        </div>
-    )
-}
--- LocalD/pbp_flutter/android/app/src/main/kotlin/com/example/pbp_flutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.pbp_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
